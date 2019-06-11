bd2fa94fd2bfce48df7c4543c4635d2545edaa50
freak4pc <freak4pc@gmail.com>
2019-06-11 23:02:30 +0300

    Add SPM Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "RxCombine",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
+    ],
+    products: [
+        .library(
+            name: "RxCombine",
+            targets: ["RxCombine"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "5.0.0")
+    ],
+    targets: [
+        .target(
+            name: "RxCombine",
+            dependencies: ["RxSwift", "RxRelay"],
+            path: "Sources")
+    ]
+)
