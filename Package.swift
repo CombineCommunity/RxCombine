57f16f9c64004956186d2dd49397e2dee2c269b9
Ethan Wong <e1hanW0ng@gmail.com>
2021-08-22 01:07:34 +0800

    Raise SPM target platform version to satisfy requirements of Combine

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "RxCombine",
     platforms: [
-        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v3)
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
         .library(
