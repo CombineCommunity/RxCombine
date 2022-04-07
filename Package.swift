88d51d2dfa8d145043922b6cff14088e84218b1b
Jordan Kay <jordan@fleuronic.com>
2022-04-07 18:41:38 -0400

    Simplify Package.swift

--- Package.swift
+++ Package.swift
@@ -2,35 +2,16 @@
 
 import PackageDescription
 
-#if os(Linux)
-let package = Package(
-    name: "RxCombine",
-    platforms: [
-        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v3)
-    ],
-    products: [
-        .library(
-            name: "RxCombine",
-            targets: ["RxCombine"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
-		.package(url: "https://github.com/cx-org/CombineX", from: "0.4.0")
+var dependencies: [Package.Dependency] = [
+	.package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0")
+]
+var targetDependencies: [Target.Dependency] = ["RxSwift", "RxRelay"]
 
-    ],
-    targets: [
-        .target(
-            name: "RxCombine",
-            dependencies: ["RxSwift", "RxRelay", "CombineX"],
-            path: "Sources"),
-        .testTarget(
-            name: "RxCombineTests",
-            dependencies: ["RxCombine"],
-            path: "Tests"
-        )
-    ]
-)
-#else
+#if os(Linux)
+dependencies.append(.package(url: "https://github.com/cx-org/CombineX", from: "0.4.0"))
+targetDependencies.append("CombineX")
+#endif
+
 let package = Package(
 	name: "RxCombine",
 	platforms: [
@@ -41,13 +22,11 @@ let package = Package(
 			name: "RxCombine",
 			targets: ["RxCombine"]),
 	],
-	dependencies: [
-		.package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0")
-	],
+	dependencies: dependencies,
 	targets: [
 		.target(
 			name: "RxCombine",
-			dependencies: ["RxSwift", "RxRelay"],
+			dependencies: targetDependencies,
 			path: "Sources"),
 		.testTarget(
 			name: "RxCombineTests",
@@ -56,4 +35,3 @@ let package = Package(
 		)
 	]
 )
-#endif
